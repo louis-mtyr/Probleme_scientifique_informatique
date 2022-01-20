a3ffd5f54c66669638e9adc45dc2ec61cc1baaed
martyr <martyr@DESKTOP-7J32667>
2022-01-20 14:51:02 +0100

    test

---
+++ Pb_Scientifique_Info/Pb_Scientifique_Info/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pb_Scientifique_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("help");
+        }
+    }
+}
