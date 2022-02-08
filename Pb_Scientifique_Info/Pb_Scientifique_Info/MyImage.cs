6e450c1469c7c09a6f03142804e6d4a9f73cdf9c
louis-mtyr <94978517+louis-mtyr@users.noreply.github.com>
2022-02-08 13:40:22 +0100

    début td2 - lecture image

--- Pb_Scientifique_Info/Pb_Scientifique_Info/MyImage.cs
+++ Pb_Scientifique_Info/Pb_Scientifique_Info/MyImage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Pb_Scientifique_Info
 {
@@ -15,6 +16,46 @@ namespace Pb_Scientifique_Info
         private int nbBitsCouleur;
         private byte[] image;
 
+        public string Myfile
+        {
+            get { return this.myfile; }
+        }
+
+        public string TypeImage
+        {
+            get { return this.typeImage; }
+        }
+
+        public int TailleFichier
+        {
+            get { return this.tailleFichier; }
+        }
+
+        public int TailleOffset
+        {
+            get { return this.tailleOffset; }
+        }
+
+        public int HauteurImage
+        {
+            get { return this.hauteurImage; }
+        }
+
+        public int LargeurImage
+        {
+            get { return this.largeurImage; }
+        }
+
+        public int NbBitsCouleur
+        {
+            get { return this.nbBitsCouleur; }
+        }
+
+        public byte[] Image
+        {
+            get { return this.image; }
+        }
+
         public MyImage(string typeImage, int tailleFichier, int tailleOffset, int hauteurImage, int largeurImage, int nbBitsCouleur, byte[] image)
         {
             this.typeImage = typeImage;
@@ -28,7 +69,33 @@ namespace Pb_Scientifique_Info
 
         public MyImage(string myfile)
         {
+            byte[] tab = File.ReadAllBytes(myfile);
+            this.typeImage = "Pas BitMap";
+            if (tab[0] == (byte)66) if (tab[1] == (byte)77) this.typeImage = "BitMap";
 
+            byte[] tailleFichierEndian = new byte[4];
+            for (int i = 2; i < 6; i++) tailleFichierEndian[i-2] = tab[i];
+            this.tailleFichier = Convert_Endian_To_Int(tailleFichierEndian);
+
+            byte[] tailleOffsetEndian = new byte[4];
+            for (int i = 14; i < 18; i++) tailleOffsetEndian[i - 14] = tab[i];
+            this.tailleOffset = Convert_Endian_To_Int(tailleOffsetEndian);
+
+            byte[] hauteurImageEndian = new byte[4];
+            for (int i = 18; i < 22; i++) hauteurImageEndian[i - 18] = tab[i];
+            this.hauteurImage = Convert_Endian_To_Int(hauteurImageEndian);
+
+            byte[] largeurImageEndian = new byte[4];
+            for (int i = 22; i < 26; i++) largeurImageEndian[i - 22] = tab[i];
+            this.largeurImage = Convert_Endian_To_Int(largeurImageEndian);
+
+            byte[] nbBitsCouleurEndian = new byte[2];
+            for (int i = 28; i < 30; i++) nbBitsCouleurEndian[i - 28] = tab[i];
+            this.nbBitsCouleur = Convert_Endian_To_Int(nbBitsCouleurEndian);
+
+            byte[] limage = new byte[hauteurImage * largeurImage * 3];
+            for (int i = 54; i < tab.Length; i++) limage[i - 54] = tab[i];
+            this.image = limage;
         }
 
         public void From_Image_To_File(string file)
@@ -41,7 +108,7 @@ namespace Pb_Scientifique_Info
             double res = 0;
             for (int i=0; i<tab.Length; i++)
             {
-                res += Math.Pow(tab[i], i);
+                res += tab[i]*Math.Pow(256, i);
             }
             return (int)res;
         }
--- Pb_Scientifique_Info/Pb_Scientifique_Info/Program.cs
+++ Pb_Scientifique_Info/Pb_Scientifique_Info/Program.cs
@@ -6,9 +6,29 @@ namespace Pb_Scientifique_Info
     {
         static void Main(string[] args)
         {
-            //test
-            Console.Write("help");
-            //test
+            MyImage test = new MyImage("Test001.bmp");
+            /*Console.WriteLine("Header :");
+            for (int i = 0; i < 14; i++) Console.Write(test.Image[i] + " ");
+            Console.WriteLine("\n\nHeader Info :");
+            for (int i = 14; i < 54; i++) Console.Write(test.Image[i] + " ");
+            Console.WriteLine("\n\nImage :");
+            for (int i = 0; i < test.Image.Length; i += 60)
+            {
+                for (int j = i; j < i + 60; j++)
+                    Console.Write(test.Image[j] + " ");
+                Console.WriteLine();
+            }*/
+            Console.WriteLine(test.TypeImage);
+            Console.WriteLine(test.TailleFichier);
+            Console.WriteLine(test.TailleOffset);
+            Console.WriteLine(test.HauteurImage);
+            Console.WriteLine(test.LargeurImage);
+            Console.WriteLine(test.NbBitsCouleur);
+            for (int i=0; i<test.Image.Length; i++)
+            {
+                if ((i % (test.LargeurImage * 3) == 0) && i!=0) Console.WriteLine();
+                Console.Write(test.Image[i] + " ");
+            }
         }
     }
 }
