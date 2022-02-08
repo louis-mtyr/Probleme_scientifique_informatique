fdb7c8695ed2a6d57cf0a2af49576527d3bda9a9
louis-mtyr <94978517+louis-mtyr@users.noreply.github.com>
2022-02-08 18:44:41 +0100

    Inverse + Noir et blanc (pas  dingue)

--- Pb_Scientifique_Info/Pb_Scientifique_Info/Program.cs
+++ Pb_Scientifique_Info/Pb_Scientifique_Info/Program.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Media;
+using System.ComponentModel;
 
 namespace Pb_Scientifique_Info
 {
     class Program
     {
-        /*static public void NoirEtBlanc(MyImage image)
+        static public void NoirEtBlanc(MyImage image)
         {
-            for (int i=0; i<image.Image.Length; i+=3)
+            for (int i=0; i<image.Image.Length; i++)
             {
-                image.Image[i] = Convert.ToByte(255 - (int)image.Image[i]);
-                image.Image[i+1] = Convert.ToByte(255 - (int)image.Image[i]);
-                image.Image[i+2] = Convert.ToByte(255 - (int)image.Image[i]);
+                image.Image[i].R = (byte)(255 - image.Image[i].R);
+                image.Image[i].G = (byte)(255 - image.Image[i].R);
+                image.Image[i].B = (byte)(255 - image.Image[i].R);
             }
         }
 
         static public void Inverse(MyImage image)
         {
-            for (int i = 0; i < image.Image.Length; i += 3)
+            for (int i = 0; i < image.Image.Length; i++)
             {
-                image.Image[i] = Convert.ToByte(255 - (int)image.Image[i]);
-                image.Image[i + 1] = Convert.ToByte(255 - (int)image.Image[i+1]);
-                image.Image[i + 2] = Convert.ToByte(255 - (int)image.Image[i+2]);
+                image.Image[i].R = (byte)(255 - image.Image[i].R);
+                image.Image[i].G = (byte)(255 - image.Image[i].G);
+                image.Image[i].B = (byte)(255 - image.Image[i].B);
             }
-        }*/
+        }
 
         static void Main(string[] args)
         {
@@ -42,9 +50,12 @@ namespace Pb_Scientifique_Info
             test.From_Image_To_File("testingTD2.bmp");
             Console.WriteLine();
             Console.WriteLine();
-            /*Process.Start("testingTD2.bmp");
+            //Process.Start("testingTD2.bmp");
+            NoirEtBlanc(test);
+            test.From_Image_To_File("testingTD2_NoirEtBlanc.bmp");
             Inverse(test);
-            Console.WriteLine("type d'image : " + test.TypeImage);
+            test.From_Image_To_File("testingTD2Inverse.bmp");
+            /*Console.WriteLine("type d'image : " + test.TypeImage);
             Console.WriteLine("taille du fichier : " + test.TailleFichier);
             Console.WriteLine("taille offset : " + test.TailleOffset);
             Console.WriteLine("hauteur de l'image : " + test.HauteurImage);
